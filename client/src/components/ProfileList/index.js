9aa3ae5e8cf5a8be939774fb3bbf54a074fa041e
Steve Krenn <stevekrenn@gmail.com>
2023-02-15 20:24:27 -0600

    prototype article div centered and finished

--- client/src/components/ProfileList/index.js
+++ client/src/components/ProfileList/index.js
@@ -12,13 +12,47 @@ const ProfileList = ({ profiles, title }) => {
         {profiles &&
           profiles.map((profile) => (
             <div key={profile._id} className="col-12 col-xl-6">
-              <div className="card mb-3">
+
+              <div className="card mb-3 d-flex justify-content-center">
+
+                <div className="card-header bg-dark text-light p-2 m-0 inline-block text-center">
+                  <h4>
+                    ARTICLE TITLE TEST #2 </h4>
+
+                  <img src="https://via.placeholder.com/250" />
+                  <br />
+                  <span className="text-white" style={{ fontSize: '1rem' }}>
+                    ARTICLE BODY PLACEHOLDER
+                  </span>
+                </div>
+
                 <h4 className="card-header bg-dark text-light p-2 m-0">
                   ARTICLE TITLE TEST #2 <br />
+                  <img src="https://via.placeholder.com/250" />
+                  <br />
                   <span className="text-white" style={{ fontSize: '1rem' }}>
                     ARTICLE BODY PLACEHOLDER
                   </span>
                 </h4>
+
+                <h4 className="card-header bg-dark text-light p-2 m-0">
+                  ARTICLE TITLE TEST #2 <br />
+                  <img src="https://via.placeholder.com/250" />
+                  <br />
+                  <span className="text-white" style={{ fontSize: '1rem' }}>
+                    ARTICLE BODY PLACEHOLDER
+                  </span>
+                </h4>
+
+                <h4 className="card-header bg-dark text-light p-2 m-0">
+                  ARTICLE TITLE TEST #2 <br />
+                  <img src="https://via.placeholder.com/250" />
+                  <br />
+                  <span className="text-white" style={{ fontSize: '1rem' }}>
+                    ARTICLE BODY PLACEHOLDER
+                  </span>
+                </h4>
+
               </div>
             </div>
           ))}
