10bb5cff335c16fbb5472b38412cb024d4a509dd
Mitchell Lemieux <ddwk21@gmail.com>
2023-02-20 19:24:54 -0600

    fetching random ID and xml parsing logic

--- server/routes/randomArticle.js
+++ server/routes/randomArticle.js
@@ -5,7 +5,7 @@ router.get('/', async(req, res) => {
     try {
         
         let list = ArticleList.findOne({'category': req.params.cat})
-        let id = list.articles[0].articles[Math.floor(Math.random*(list.articles[0].totalCount-1))].articleId//get random index of category
+        let id = list.articles[0].articles[Math.floor(Math.random*(list.articles.length-1))].articleId//get random index of category
 
         res.json(id)
     }catch(err){
