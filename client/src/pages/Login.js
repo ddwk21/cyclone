582095f261e31c22ccecf55263a8ca2858e3fc6a
Jeremy Bukofzer <jlbukofzer@gmail.com>
2023-02-21 22:46:45 -0600

    fixes focus issue on login/signup

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -75,6 +75,7 @@ const Login = (props) => {
                   style={{ cursor: 'pointer' }}
                   type="submit"
                   >
+
             Submit
         </button>
     </form>
